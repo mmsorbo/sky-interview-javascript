dbf16c2a686991d1b4970dd3ca9472f6158e4a92
mmsorbo <m.m.sorbo@gmail.com>
2023-11-24 11:36:53 +0100

    Update e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -3,6 +3,9 @@ Create new cy command to get Futurama info
 - check if result status is 200 
 - check if result is array with 1 element
 - check if the first element in result has id  equal to 1
+
+To run open new terminal and run npx cypress run --e2e
+
 */
 
 Cypress.Commands.add("getFuturamaInfo", () => {
